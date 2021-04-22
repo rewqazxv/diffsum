638d8ac53e7ccb2f20b12250cf3d3cb9471433bd
rewqazxv <41326691+rewqazxv@users.noreply.github.com>
2021-04-23 02:47:08 +0800

    checksum.sh: output log to target folder

--- checksum.sh
+++ checksum.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 SUM='sha1sum'
 LOC=${1:-.}
 
-exec find $LOC -type f ! -name "*.$SUM" -print0 | xargs -0 $SUM > `date +%Y%m%d%H%M%S`.$SUM
+exec find $LOC -type f ! -name "*.$SUM" -print0 | xargs -0 $SUM > $LOC/`date +%Y%m%d%H%M%S`.$SUM
